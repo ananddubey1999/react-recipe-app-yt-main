8471d6c7d08f1209f680f8b04c64873a49cca026
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-12-08 11:29:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './HomePage/Home'
-import './App.css'
-import RecipeId from './RecipeIDPage/RecipeId'
-import Category from './Category/Category'
-import SearchElement from './SearchElement/SearchElement'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './HomePage/Home';
+import './App.css';
+import RecipeId from './RecipeIDPage/RecipeId';
+import Category from './Category/Category';
+import SearchElement from './SearchElement/SearchElement';
+import generateSitemap from './sitemapGenerator'; // Import your sitemap generator
 
 const App = () => {
+  // Generate sitemap
+  const urls = generateSitemap();
+
   return (
-  <>
-  <Router>  
-    <Routes>
-      <Route path='/' element={ <Home />} />
-      <Route path='/:idMeal' element={ <RecipeId />} />
-      <Route path='/category/:name' element={ <Category />} />
-      <Route path='/search/:searchTerm' element={ <SearchElement />} />
-    </Routes>
-  </Router>
- 
-  </>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/:idMeal' element={<RecipeId />} />
+          <Route path='/category/:name' element={<Category />} />
+          <Route path='/search/:searchTerm' element={<SearchElement />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
-
-// Part 2
-
-// Note - when we applied the css on App.js(App.css) the it will work same for all the componant of react
+export default App;
---
+++ src/sitemapGenerator.js
@@ -0,0 +1,23 @@
+// sitemapGenerator.js
+
+const generateSitemap = () => {
+    const baseUrl = 'https://yourwebsite.com'; // Replace with your website's URL
+    const urls = [
+      `${baseUrl}/`,
+      `${baseUrl}/category/indian`,
+      `${baseUrl}/category/american`,
+      `${baseUrl}/category/british`,
+      `${baseUrl}/category/chinese`,
+      `${baseUrl}/category/thai`,
+      // Add more category URLs as needed
+      // You may also include dynamic URLs for RecipeId and SearchElement pages if they have specific URLs
+    ];
+  
+    // Assuming you have a fixed list of categories, add their URLs above.
+    // If the RecipeId or SearchElement pages have specific URLs based on IDs or search terms, generate those URLs here too.
+  
+    return urls;
+  };
+  
+  export default generateSitemap;
+  
